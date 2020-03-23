9db9c83d5a6a061e48eb1109f351edaea162caa9
garebare <garedaiki@outlook.jp>
2020-03-23 18:17:10 +0900

    add:args

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,41 @@
-use koto::lexer::Lexer;
-use koto::lexer::Token;
+use std::env;
+
 use koto::ast::Parsing;
 use koto::interpreter;
+use koto::lexer::Lexer;
+use koto::lexer::Token;
 
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 
 fn main() {
-    let mut index = 0;
-    let path = "./programs/fuga.koto";
-    let file = File::open(path).expect("file not found");
-    let mut file_buffer = BufReader::new(&file);
-    let mut content = String::new();
-    file_buffer.read_to_string(&mut content);
+    let args: Vec<String> = env::args().collect();
 
-    let len = content.len();
-    let mut tokens:Vec<Token::TokenValue> = Vec::new();
-    loop {
-        if index >= len {
-            break;
+    if &args[1] == "run" {
+        let mut index = 0;
+        let path = &args[2];
+        let file = File::open(path).expect("file not found");
+        let mut file_buffer = BufReader::new(&file);
+        let mut content = String::new();
+        file_buffer.read_to_string(&mut content);
+        let len = content.len();
+        let mut tokens: Vec<Token::TokenValue> = Vec::new();
+        loop {
+            if index >= len {
+                break;
+            }
+            let (result, continue_index) = Lexer::get(&content, index);
+            index = continue_index;
+            println!("{:?}", result);
+            tokens.push(result);
         }
-
-        let (result, continue_index) = Lexer::get(&content, index);
-
-        index = continue_index;
+        let result = Parsing::parsing(&mut tokens);
         println!("{:?}", result);
-        tokens.push(result);
+
+        interpreter::Interpreter::run(result);
+    } else {
+        println!("file run");
+        println!("./koto run [file name]");
     }
-
-    let result = Parsing::parsing(&mut tokens);
-    println!("{:?}", result);
-
-    interpreter::Interpreter::run(result);
 }
