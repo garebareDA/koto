f2ca140dc46786c387e80596e340fc51ff30fae9
garebare <garedaiki@outlook.jp>
2020-06-12 22:32:38 +0900

    add fn:compile

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ fn main() {
         let result = interpreter::interpreters::read_file(path);
         println!("{:?}", result);
         interpreter::interpreters::run(result);
+    }else if args.len() == 3 && &args[1] == "compile"{
+        //TODO中間言語(C言語)にコンパイルして実行する
     } else {
         println!("file run");
         println!("./koto run [file name]");
