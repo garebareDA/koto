90316243c75de70ff7a96aec1bbc3f44beac1bb8
garebare <garedaiki@outlook.jp>
2020-06-15 17:33:59 +0900

    fix:parsing

--- build/build.c
+++ build/build.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-int main()
-{
-  char string[] = "a";
-  int number = 1;
-
-  char stdin[1000];
-  scanf("%s", stdin);
+int main() {
+char string[] = "a";
+int number = 1;
+bool a = false;
+char stdin[1000];
+scanf("%s",stdin);
   printf("%s\n", "hello world");
   printf("%s\n", stdin);
   return 0;
