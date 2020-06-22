ab14140a4a91732d4de0ba73867841653522db8c
garebare <garedaiki@outlook.jp>
2020-06-23 03:57:29 +0900

    add:fors_juge

--- src/compiler/fors.rs
+++ src/compiler/fors.rs
@@ -5,6 +5,32 @@ impl Compile {
   pub(crate) fn fors_write(&mut self, fors: &asts::ForAST){
     self.variable.vec_push();
 
-    
+    let variant = &fors.init_var[0];
+    let judge = &fors.init_var[1];
+    let loop_for = &fors.init_var[2];
+
+    self.write("for(");
+
+    match variant {
+      asts::Types::Variable(vars) => {
+        self.variable_wirte(vars);
+      }
+
+      _ => {
+        //error
+      }
+    }
+
+    match judge {
+
+
+      _ => {
+        //error
+      }
+    }
+  }
+
+  fn fors_juge() {
+
   }
 }
