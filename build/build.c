f3c42115bc182f498afeeda7fe8545286c9d7fdc
garebare <garedaiki@outlook.jp>
2020-06-27 03:16:52 +0900

    compiler.koto

--- build/build.c
+++ build/build.c
@@ -1,32 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-int import_hoge_function(int n)
+int function(int n,int a)
 {
 int tpm0 = n<3;;
 if(atoi(tpm0)){
 return 1;
 }
-int b = ;;
+int b = 1;
 return b;
 }
-void import_hoge_a()
-{
-printf("%s\n", "hello");
-}
 int main() {
+char string[] = "a";
 int number = 1;
-char string[34] = "\0";
-snprintf(string, 34, "%s%d%s%d","hello, world",1+1+1,"mozirel",123);
-printf("%s\n", string);
-int vector[5] = {1,2,3,4,5,};
-int index = vector;int index = vector[1];
-printf("%d\n", index);
-int cal = number+1-1/2*5;;
-cal++;
-printf("%d\n", cal);
-int result = index;
-result = cal;
-printf("%d\n", result);
-for(int i = 0;i>5;i++)
+int bools = 0;
+int aa = number+1+1+1*1;;
+int tpm1 = number<aa;;
+if(atoi(tpm1)){
+printf("%s\n", "if!");
+}
+for(int a = 1;a<5;a++)
 {
-int reslut = import_hoge_function(
+printf("%s\n", "for!");
+}
+int tpm2 = 1<0+6;;
+if(atoi(tpm2)){
+}
+printf("%s\n", "hello world");
+printf("%d\n", aa);
+int c = function(number,1);
+printf("%d\n", c);
+int vector[5] = {1,2,3,4,5,};
+int v = vector[2];
+printf("%d\n", v);
+  return 0;
+}
