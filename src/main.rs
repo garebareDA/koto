6f28f05d78f6670250567d9f9bc38c8dc9207d9d
garebare <garedaiki@outlook.jp>
2020-07-04 16:04:08 +0900

    wasm build

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
+extern crate web_sys;
+extern crate wasm_bindgen;
+
 use koto::compiler;
 use koto::interpreter;
-use std::env;
+use koto::lexer::lexers;
+use koto::ast::parsing;
 
-use std::process::{Command, Stdio};
+use std::env;
+use std::process::Command;
+use wasm_bindgen::prelude::*;
+
 //関数の書き込みをなんとかする
 
 fn main() {
@@ -34,4 +41,25 @@ fn main() {
         println!("file run");
         println!("./koto run [file name]");
     }
+}
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace=console)]
+    fn log(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn run() {
+    let document = web_sys::window().unwrap().document().unwrap();
+    document.get_element_by_id("result").unwrap().set_node_value(Some(""));
+
+    let val = document.get_element_by_id("code").unwrap().node_value().unwrap();
+    log(&val);
+
+    let mut lexer = lexers::Lexer::new(&val);
+    let tokens = lexer.start();
+    let mut pars = parsing::Parsing::new(&tokens);
+    let result = pars.parsing();
+    interpreter::interpreters::run(result);
 }
