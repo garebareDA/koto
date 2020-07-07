0bd233df3ad2cdce4da387c1abc2c7d0b2004d8b
garebare <garedaiki@outlook.jp>
2020-07-07 20:18:09 +0900

    add:firebase

--- js/server.js
+++ js/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
-express.static.mime.define({'application/wasm': ['wasm']})
+const path = require('path');
+express.static.mime.define({'application/wasm': ['wasm']});
 var app = express();
 
-app.use('/', express.static(__dirname + '/dist'));
-
+app.use('/', express.static(path.resolve(__dirname, "../dist")));
 
 app.listen(process.env.PORT, function () {
-  console.log('Example app listening on port 8000!')
+  console.log('Example app listening on port 3000!')
 })
